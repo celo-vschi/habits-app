a626844bb608d7ab515041062acae0c0c226f177
andrei.celovschi <andrei.celovschi.bp@nttdata.ro>
2019-12-06 18:58:59 +0200

    added ability to select a date

--- src/components/HabitDayPicker.js
+++ src/components/HabitDayPicker.js
@@ -4,15 +4,26 @@ import DayPicker from 'react-day-picker';
 import * as utils from '../utils/utils';
 
 export class HabitDayPicker extends React.Component {
+    state = {
+        selectedDay: null
+    };
+
+    handleDayClick = (day, { selected }) => {
+        this.setState(() => ({ selectedDay: selected ? undefined : day }));
+    };
+
     modifiers = {
         green: this.props.calendarData.green,
         orange: this.props.calendarData.orange,
         red: this.props.calendarData.red
     };
+
     render() {
         return (
             <DayPicker
                 modifiers={this.modifiers}
+                selectedDays={this.state.selectedDay}
+                onDayClick={this.handleDayClick}
                 onDayclassName="calendar" />
         );
     }

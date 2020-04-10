72b082481d2a36527ffd9939dbf36d91d7d8ac22
Celo <celovschiandrei@gmail.com>
2020-04-10 18:39:12 +0200

    added custom component for habit titles

--- src/components/HabitListCheck.js
+++ src/components/HabitListCheck.js
@@ -3,19 +3,29 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import HabitListItemCheck from './HabitListItemCheck';
 import * as utils from '../utils/utils';
+import HabitListItemTitle from './HabitListItemTitle';
 
 export const HabitListCheck = (props) => (
     <div className="widget">
         {props.habits.length > 0 ?
-            (props.habits.map((habit, i) => (
-                <HabitListItemCheck
-                    key={habit.id}
-                    last={props.habits.length === i + 1}
-                    {...habit}
-                    date={props.date}
-                    props={props}
-                />
-            ))) : (
+            (props.habits.map((habit, i) => {
+                return habit.name.includes('_') ? (
+                    <HabitListItemTitle
+                        key={habit.id}
+                        {...habit}
+                        date={props.date}
+                        props={props}
+                    />
+                ) : (
+                        <HabitListItemCheck
+                            key={habit.id}
+                            last={props.habits.length === i + 1}
+                            {...habit}
+                            date={props.date}
+                            props={props}
+                        />
+                    )
+            })) : (
                 <Link className="button--link" to="/create">
                     <p className="widget__message">You have no habits. Click here to add a habit.</p>
                 </Link>

60a90862463ef9692a2d0c19c22c2ee22eff786b
Celo <celovschiandrei@gmail.com>
2020-04-10 15:22:34 +0200

    improved the header alignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,14 @@ export const Header = ({ startLogout }) => (
     <header className="header">
         <div className="content-container">
             <div className="header__content">
-                <Link className="header__title" to="/dashboard">
-                    <h1>Dashboard</h1>
-                </Link>
-                <Link className="header__title" to="/check">
-                    <h3>Habits Check</h3>
-                </Link>
+                <div className="header-title-container">
+                    <Link className="header__title" to="/dashboard">
+                        <h1>Dashboard</h1>
+                    </Link>
+                    <Link className="header__title" to="/check">
+                        <h3>Habits Check</h3>
+                    </Link>
+                </div>
                 <button className="header__button" onClick={startLogout}>Logout</button>
             </div>
         </div>

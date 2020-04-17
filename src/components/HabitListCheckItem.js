b3effbe4307e0896f73765e91ca3c51fcc527c79
Celo <celovschiandrei@gmail.com>
2020-04-17 22:45:34 +0200

    added half-check

--- src/components/HabitListCheckItem.js
+++ src/components/HabitListCheckItem.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { startMarkHabit } from '../actions/habits';
+import { startMarkHabit, startEditHabit } from '../actions/habits';
 import * as utils from '../utils/utils';
+import moment from 'moment';
 
 export class HabitListCheckItem extends React.Component {
     markDone = () => {
         const id = this.props.id;
         const date = this.props.date;
         this.props.startMarkHabit(id, date, true);
+
+        const halfDone = {
+            halfDone: true
+        };
+        this.props.startEditHabit(this.props.id, halfDone);
     }
 
     markUndone = () => {
         const id = this.props.id;
         const date = this.props.date;
         this.props.startMarkHabit(id, date, false);
+    }
+
+    markHalfDone = (e) => {
+        const halfDoneValue = !e.target.className.includes('checked');
+        const halfDone = {
+            halfDone: halfDoneValue
+        }
+        this.props.startEditHabit(this.props.id, halfDone);
     }
 
     handleChange = (e) => {
@@ -26,6 +40,7 @@ export class HabitListCheckItem extends React.Component {
     }
 
     render() {
+        const showHalfCheck = !this.props.done && this.props.date == moment().format('YYYY-MM-DD');
         return (
             <div className={this.props.last ? "habit habit__last" : "habit"}>
                 <label className="checkbox-container">
@@ -43,13 +58,24 @@ export class HabitListCheckItem extends React.Component {
                         {this.props.streak}
                     </span>
                 </label>
+
+                {showHalfCheck &&
+                    <div className="habit__right">
+                        <p
+                            className={this.props.halfDone ? "half-check__checked" : "half-check"}
+                            onClick={this.markHalfDone}>
+                            x
+                    </p>
+                    </div>
+                }
             </div>
         );
     }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    startMarkHabit: (id, date, done) => dispatch(startMarkHabit({ id, date, done }))
+    startMarkHabit: (id, date, done) => dispatch(startMarkHabit({ id, date, done })),
+    startEditHabit: (id, halfDone) => { dispatch(startEditHabit(id, halfDone)) }
 });
 
 const mapStateToProps = (state, props) => ({

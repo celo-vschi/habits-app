788859c3f057869703a403f05ead9c736a28d3b9
Celo <celovschiandrei@gmail.com>
2020-05-05 18:59:44 +0200

    second iteration and the collapsiblity is finished <3

--- src/components/DoneHabitListCheck.js
+++ src/components/DoneHabitListCheck.js
@@ -1,65 +1,68 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { AiFillCaretDown } from "react-icons/ai";
+import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 import HabitListCheckItem from './HabitListCheckItem';
 import HabitListCheckTitle from './HabitListCheckTitle';
 import * as utils from '../utils/utils';
 
-export const DoneHabitListCheck = (props) => {
-    const doneHabits = props.habits.filter((habit) => habit.done);
-    if (doneHabits.length > 0) {
-        return (
-            <div>
-                <div className="widget-header">
-                    <div className="widget-header-item">
-                        <button className="button--link collapse-button" onClick={collapse}>
-                            <AiFillCaretDown size={25} />
-                        </button>
-                    </div>
-                    <div className="widget-header-item">
-                        <h3>Finished Habits</h3>
-                    </div>
-                </div>
-                <div className="widget collapsible">
-                    {doneHabits.map((habit, i) => {
-                        return habit.name.includes('_') ? (
-                            <HabitListCheckTitle
-                                key={habit.id}
-                                {...habit}
-                                date={props.date}
-                                props={props}
-                            />
-                        ) : (
-                                <HabitListCheckItem
-                                    key={habit.id}
-                                    last={doneHabits.length === i + 1}
-                                    {...habit}
-                                    date={props.date}
-                                    props={props}
-                                />
-                            )
-                    })}
-                </div>
-            </div>
-        )
+export class DoneHabitListCheck extends React.Component {
 
-    } else {
-        return null;
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: false
+        };
+    };
+
+    toggleCollapsing = () => {
+        this.setState((prevState) => ({ collapsed: !prevState.collapsed }));
     }
 
-};
+    render = (props) => {
+        const doneHabits = this.props.habits.filter((habit) => habit.done);
+        if (doneHabits.length > 0) {
+            return (
+                <div>
+                    <div className="widget-header">
+                        <div className="widget-header-item">
+                            <button className="button--link" onClick={this.toggleCollapsing}>
+                                {this.state.collapsed ? <AiFillCaretUp size={25} /> : <AiFillCaretDown size={25} />}
+                            </button>
+                        </div>
+                        <div className="widget-header-item">
+                            <h3>Finished Habits</h3>
+                        </div>
+                    </div>
+                    <div className="widget">
+                        <div style={{ display: this.state.collapsed ? 'block' : 'none' }}>
+                            {doneHabits.map((habit, i) => {
+                                return habit.name.includes('_') ? (
+                                    <HabitListCheckTitle
+                                        key={habit.id}
+                                        {...habit}
+                                        date={this.props.date}
+                                        props={props}
+                                    />
+                                ) : (
+                                        <HabitListCheckItem
+                                            key={habit.id}
+                                            last={doneHabits.length === i + 1}
+                                            {...habit}
+                                            date={this.props.date}
+                                            props={props}
+                                        />
+                                    )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            )
 
-const collapse = () => {
-    var collapsible = document.getElementsByClassName("collapsible")[0];
-    var children = collapsible.childNodes;
-    for (var i = 0; i < children.length; i++) {
-        var e = children[i];
-        if (e.style.display === "block") {
-            e.style.display = "none";
         } else {
-            e.style.display = "block";
+            return null;
         }
-    }
+
+    };
 }
 
 const mapStateToProps = (state) => {
